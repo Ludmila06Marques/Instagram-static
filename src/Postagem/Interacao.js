6ae1e4964f24543a1a74a8a26e726700ec7830b4
Lud <instimila@gmail.com>
2022-05-08 17:08:12 +0200

    Implementacao do botao curtir

--- src/Postagem/Interacao.js
+++ src/Postagem/Interacao.js
@@ -1,10 +1,28 @@
+import React from "react"
+
 
 export default function Interacao(props){
+
+const [ classButton, setClassButton]= React.useState("icone2")
+const [ nameButton , setNameButton]=React.useState("heart-outline")
+
+function curtir(){
+   
+    if(classButton=="icone2 preenchido" ){
+        setClassButton("icone2 ") 
+        setNameButton("heart-outline")     
+    } else{
+        setClassButton("icone2 preenchido")
+        setNameButton("heart")
+    }
+  
+}
+
     return(
         <div class="interacao">
                               
         <div class="interacao2">
-            <ion-icon class="icone2" name="heart-outline"></ion-icon>
+        <ion-icon onClick={curtir}class={classButton} name={nameButton}></ion-icon>
             <ion-icon class="icone2"name="chatbubble-outline"></ion-icon>
             <ion-icon class="icone2"name="paper-plane-outline"></ion-icon>
         </div>
